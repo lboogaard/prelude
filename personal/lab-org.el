78318d5c2c2d6068e311ce7c07177355446c201f
Leindert Boogaard <boogaard@mpia.de>
2021-09-17 11:20:44 +0200

    org longer multi-line emphasis and also use Skim

--- personal/lab-org.el
+++ personal/lab-org.el
@@ -1,7 +1,7 @@
 (with-eval-after-load 'org
   ;; Allow multiple line Org emphasis markup.
   ;; http://emacs.stackexchange.com/a/13828/115
-  (setcar (nthcdr 4 org-emphasis-regexp-components) 5) ;Up to 5 lines, default is 1
+  (setcar (nthcdr 4 org-emphasis-regexp-components) 20) ;Up to 20 lines, default is 1
   ;; Below is needed to apply the modified `org-emphasis-regexp-components'
   ;; settings from above.
   (org-set-emph-re 'org-emphasis-regexp-components org-emphasis-regexp-components))
@@ -9,3 +9,4 @@
 ;; update the default width=.9\linewidth in org-mode latex export
 (setq org-latex-image-default-width "0.95\\linewidth")
 
+(add-to-list 'org-file-apps '("\\.pdf" . "open -a Skim %s"))
